546e88b718419f39df8ae60922d7039f5762c6bf
Kahnu Charan Swain <djkanha044@gmail.com>
2023-07-11 21:24:19 +0530

    Update dashicons

--- functions.php
+++ functions.php
@@ -47,12 +47,13 @@ function your_theme_name_child_assets()
     if (!is_admin()) {
         wp_enqueue_style('your_theme_name_child_css', trailingslashit(get_stylesheet_directory_uri()) . 'style.css');
     }
+    wp_enqueue_style('dashicons');
     wp_enqueue_script("your_theme_name-main-js", get_stylesheet_directory_uri() . "/assets/js/main.js", array('jquery'), THEME_VERSION);
 }
 
 add_action('wp_enqueue_scripts', 'your_theme_name_child_assets', 99);
 
-/* Apply style for Gutenburge editor */
+/* Apply style for Gutenberg editor */
 add_action('enqueue_block_editor_assets', function () {
     wp_enqueue_style('your_theme_name_gblock_css', get_stylesheet_directory_uri() . "/assets/css/block-editor.css", false, '1.0', 'all');
 });
